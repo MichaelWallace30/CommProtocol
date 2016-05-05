c3eaeb150ce9501df368af03cfd41f61714e2007
jessieeisenberg <jessieeisenberg@AlexDebian.JessieEisenberg.edu>
2016-05-05 16:52:40 -0700

    add architecture and os separation

---
+++ architecture/os/include_defines.h
@@ -0,0 +1,11 @@
+/*
+ Include Defines available for the operating system build.
+ */
+#ifndef __INCLUDE_DEFINES_H
+#define __INCLUDE_DEFINES_H
+
+#include "arch.h"
+
+
+
+#endif // __INCLUDE_DEFINES_H
