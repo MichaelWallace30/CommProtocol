1af08c18fdcd74ba2f9ede99e6803086f17e0d35
MicahaelWallace30 <Michael.Wallace30@gmail.com>
2016-05-10 17:37:27 -0700

    Added thread loop

---
+++ comms.cpp
@@ -0,0 +1,85 @@
+#include "comms.h"
+
+
+
+/******************* Private *******************/
+
+
+/**
+Helper function to convert between C++ and C function signatures
+due to casting as a class member being incompatible with C style
+thread creation APIs. Static linkage helps with that.
+*/
+void* Comms::commuincation_helper(void* context)
+{
+	return ((Comms*)context)->commuincation_handler();
+}
+
+/** function to fun in commuincation thread */
+void* Comms::commuincation_handler()
+{
+	while (isRunning)
+	{
+		cout << "Thread Running" << endl;
+		Sleep(200);
+	}
+
+	return 0;
+}
+
+/******************* Public  *******************/
+Comms::Comms()
+{
+	isRunning = false;
+}
+
+Comms::Comms(uint8_t platformID)
+{
+	Comms();
+	setID(platformID);
+}
+
+Comms::~Comms()
+{
+	isRunning = false;
+	Sleep(300);//change to proper sleep
+}
+
+bool initConnection(uint8_t port, uint32_t baudrate = 0)
+{
+	return true;
+}
+
+bool addConnection(uint8_t destID, std::string address)
+{
+	return true;
+}
+
+bool removeConnection(uint8_t destID)
+{
+	return true;
+}
+
+bool send(uint8_t destID, char messageID[MESSAGE_ID_SIZE], uint8_t buffer[MAX_PACKET_SIZE], uint8_t messageLength)
+{
+	return true;
+}
+
+bool recv(uint8_t &sourceID, char messageID[MESSAGE_ID_SIZE], uint8_t buffer[MAX_PACKET_SIZE], uint8_t messageLength)
+{
+	return true;
+}
+
+void Comms::run()
+{
+	thread_create(&this->communication_thread, commuincation_helper, this);
+	isRunning = true;
+}
+
+void Comms::stop()
+{
+	isRunning = false;
+}
+
+
+
--- comms.h
+++ comms.h
@@ -1,26 +1,34 @@
+#ifndef COMMS_H
+#define COMMS_H
+
 #include <stdint.h>
 #include <queue>
 #include <string>
 
 #include "CommsPacket.h"//Header which declares packet structs
 
+#include "architecture\os\include_defines.h"
+
 #define MAX_CONNECTIONS 25
 
 
-#ifndef COMMS_H
-#define COMMS_H
+#include <temp.h>
+#include <iostream>
+using namespace std;
 
 class Comms{
    
 
 private:
+	/** Boolean if thread is running*/
+	bool isRunning;
     /** Add key to comnet class*/
 	uint8_t key[KEY_LENGTH];
     /** Method to read key form text file*/
     void loadKey();
 
 	/** Platform ID */
-	uint8_t plarformID;
+	uint8_t platformID;
         
 	/** Queues for application layer to push message or pop messages */
 	std::queue<uint8_t*> recvQueue;
@@ -29,17 +37,35 @@ private:
 	/** Array of connections to match destion ID to address string */
 	std::string connections[MAX_CONNECTIONS];
    
+   /** Thread to run commuincation data */
+   thread_t communication_thread;
+   
+   /** function to fun in commuincation thread */
+   void* commuincation_handler();
+   
+   /**
+   Helper function to convert between C++ and C function signatures
+   due to casting as a class member being incompatible with C style
+   thread creation APIs. Static linkage helps with that.
+   */
+   static void* commuincation_helper(void* context);
+
    //thread method
         //recv: recv, build packet, decrypt, and queue  
         //send: deque , encrypt, and send
    
-    
-    
+
 public:
+    Comms();
     /** Constuctor */
     Comms(uint8_t platformID);
     /** Destcutor */
     ~Comms();
+    
+    /** platform mutator */
+    void setID(uint8_t platformID){this->platformID = platformID;}
+    /** platform accessor */
+    uint8_t getID(){return platformID;}
     
 	//I love bool
 	//maybe enum paramter for connection type for init method?
@@ -50,8 +76,11 @@ public:
 	bool send(uint8_t destID, char messageID[MESSAGE_ID_SIZE], uint8_t buffer[MAX_PACKET_SIZE], uint8_t messageLength);
 	bool recv(uint8_t &sourceID, char messageID[MESSAGE_ID_SIZE], uint8_t buffer[MAX_PACKET_SIZE], uint8_t messageLength);
 	
-	bool run();
-	bool stop();
+	void run();
+	void stop();
+
+
+
     
 };//end Comms class
 #endif//end if COMMS_H
