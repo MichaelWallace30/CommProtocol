32066d4809c4299ab1d844d651b881633c7a30b1
MicahaelWallace30 <Michael.Wallace30@gmail.com>
2016-05-12 17:41:42 -0700

    Added doxygen style comments

--- commProto/comms.h
+++ commProto/comms.h
@@ -1,25 +1,22 @@
 #ifndef COMMS_H
 #define COMMS_H
 
-#include <stdint.h>
+#include <stdint.h>//needed for bit size variables
 #include <queue>
-#include <string>
+#include <string>//needed for addresses
 
 #include "commsPacket.h"//Header which declares packet structs
 
 #include "architecture\os\include_defines.h"
-#include "architecture\os\osThreads.h"
-#include "commsLink.h"
+#include "architecture\os\osThreads.h"//method to create threads
+#include "commsLink.h"//commincation layer interface/abstract base class
 
 
 #define MAX_BUFFER_SIZE MAX_PACKET_SIZE + HEADER_SIZE
 
 
-#include <iostream>
-using namespace std;
-
-
-
+#include <iostream>//testing only
+using namespace std;//testing only
 
 typedef enum
 {
@@ -32,30 +29,30 @@ class Comms{
    
 
 private:
-	/** Boolean if thread is running*/
+	/** Boolean if communication thread running*/
 	bool isRunning;
-    /** Add key to comnet class*/
+    /** Encryotion key*/
 	uint8_t key[KEY_LENGTH];
     /** Method to read key form text file*/
     void loadKey();
 
-	/** Data buffer to create packets with */
+	/** Data buffer to create communication stream with */
 	uint8_t data_Buf[MAX_BUFFER_SIZE];
 
 	/** Platform ID */
 	uint8_t platformID;
         
-	/** Queues for application layer to push message or pop messages */
+	/** Queues for application layer to push messages or pop messages */
 	std::queue<uint8_t*> recvQueue;
 	std::queue<uint8_t*> sendQeueu;
 
-	/** Array of connections to match destion ID to address string */
+	/** Array of connections to match destination ID to address string */
 	std::string connections[MAX_CONNECTIONS];
    
    /** Thread to run commuincation data */
    thread_t communication_thread;
    
-   /** function to fun in commuincation thread */
+   /** Method to run in commuincation thread */
    void* commuincation_handler();
    
    /**
@@ -72,20 +69,31 @@ public:
     /** Destcutor */
     ~Comms();
     
-    /** platform mutator */
+    /** platform ID mutator */
     void setID(uint8_t platformID){this->platformID = platformID;}
-    /** platform accessor */
+    /** platform ID accessor */
     uint8_t getID(){return platformID;}
     
-	//I love bool
+	/** Method will init the connection device and return false if connection failed to init properly 
+	enum connection type to use serial, udp, zigbee, ect
+	port number is the comport or udp port used will differ from windwos and unix for comports COM05 or /dev/ttyUSB05
+	baudrate is not used for for udp which is not needed but for serial and zigbee it is needed for baud rate */	
 	bool initConnection(commsLink_type_t connectionType, uint8_t port, uint32_t baudrate = 0);
-	bool addAddress(uint8_t destID, std::string address);
+
+	/** The address entered will be paired for commuincation by destination ID
+	Adding address can be a udp IPV4 or hex MAC address for zigbee
+	Adding an adress is not need for serial and will default to ""*/
+	bool addAddress(uint8_t destID, std::string address = "");
+
+	/** Removing an address removes the known assocciation of address and destination ID by using id*/
 	bool removeAddress(uint8_t destID);
     
 	bool send(uint8_t destID, uint16_t messageID, uint8_t buffer[MAX_PACKET_SIZE], uint8_t messageLength);
 	bool recv(uint8_t &sourceID, uint16_t &messageID, uint8_t buffer[MAX_PACKET_SIZE], uint8_t messageLength);
 	
+	/** Method to start communication*/
 	void run();
+	/** Method to stop communication*/
 	void stop();
 
 
