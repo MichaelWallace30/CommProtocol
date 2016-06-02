9a2f4972185ed727b9b2240ca5730936d8e61d8b
jessieEisenberg <jessieeisenberg@64JessieEisenberg64.Debian.net>
2016-06-01 22:09:26 -0700

    Added gpl license

--- tools/hash/NgcpHash.cpp
+++ tools/hash/NgcpHash.cpp
@@ -1,4 +1,30 @@
+/*
+  This file contains the NGCP researched hash implementation, so as to create
+  discrete numbers from data, which will then be used to store and separate 
+  this data from others. It attempts to avoid the most collisions possible,
+  so as to prevent any conflicts with data handling and storage. This is still
+  an ongoing research, and we will hope to continue working on improving this 
+  hash for the long run.
+
+  Non-cryptographic hash algorithm.
+
+  Copyright (C) 2016  Mario Garcia.
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include <tools/hash/NgcpHash.h>
+
 #include <architecture/macros.h>
 #include <architecture/os/arch.h>
 
@@ -34,6 +60,9 @@ ngcp_hash32_t reverse_byte(byte* _byte) {
   return hash;
 }
 
+/**
+   Check if the pointer is null.
+ */
 FORCE_INLINE
 bool valueIsNull(void* input) {
   if (input == NULL) {
@@ -71,6 +100,9 @@ ngcp_hash32_t ngcp_hash32(void* input, uint32_t length, unsigned seed = 0) {
  return hash;
 }
 
+/**
+   64-bit hash value, used if you do not need to include wider use of hashing.
+ */
 FORCE_INLINE
 ngcp_hash64_t ngcp_hash64(void* input, uint32_t length, unsigned seed = 0) {
   ngcp_hash64_t hash = 0;
@@ -94,6 +126,12 @@ ngcp_hash64_t ngcp_hash64(void* input, uint32_t length, unsigned seed = 0) {
   return hash;
 }
 
+/**
+   String hash used if string literal was declared. This does not gaurantee that the hash
+   algorithm will be able to determine exact size of the given string literal. It will, however
+   be able to accurately determine the string size if null terminator was specified at the end of the 
+   string.
+ */
 ngcp_hash32_t ngcp_hash32(const char* input, unsigned seed = 0) {
   unsigned int length = 0;
   if (valueIsNull(input)) {
@@ -105,6 +143,9 @@ ngcp_hash32_t ngcp_hash32(const char* input, unsigned seed = 0) {
   return (ngcp_hash32(input, length, seed));
 }
 
+/**
+   64-bit version of string hash.
+ */
 ngcp_hash64_t ngcp_hash64(const char* input, unsigned seed = 0) { 
   unsigned int length = 0;
   if (valueIsNull(input)) {
