b0f7637d14ef6ce5dd84ac2955e5a29215b7b6ae
jessieEisenberg <jessieeisenberg@64JessieEisenberg64.Debian.net>
2016-06-03 01:33:37 -0700

    main is back from the dead!

---
+++ CommProto/main.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <CommProto/comms.h>
+#include <architecture/os/os_threads.h>
+#include <architecture/os/include_defines.h>
+
+
+int main(int c, char** args) {
+  printf("Hello World!");
+  Comms newComms1 = Comms(1);
+  Comms newComms2 = Comms(2);
+  //newComms1.initConnection(UDP_LINK, 1338, "127.0.0.1");
+  newComms2.initConnection(UDP_LINK, 1338, "127.0.0.1");
+  
+
+  //newComms1.addAddress(1, "127.0.0.1", 1338);
+  newComms2.addAddress(1, "127.0.0.1", 1337);
+
+
+	uint8_t buf[1024] = { 1 };
+	//newComms1.send(2, 1, buf, 10);	    
+
+	newComms2.run();
+	//newComms1.run();
+
+	  uint8_t sourceID;
+	  uint16_t messageID;
+	  uint32_t messageLength;
+	  uint8_t buffer[10];
+
+	  while (true)
+	  {
+		  if (newComms2.recv(sourceID, messageID, buffer, messageLength))
+		  {
+			  printf("FOUND\n");
+		  }
+		  Sleep(100);
+		//  newComms1.send(1, 1, buf, 10);
+	  }
+  
+	return 0;
+}
