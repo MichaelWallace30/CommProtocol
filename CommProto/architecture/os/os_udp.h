71ed234b969665b616aae8d783f86d146d9566e7
MicahaelWallace30 <Michael.Wallace30@gmail.com>
2016-06-07 10:08:17 -0700

    UDP linux and windows changed
    Included typdef for closeSocket in windows/linux
    typdef  s_addr in windows
    Added header for linux htons
    removed linux headers

--- CommProto/architecture/os/os_udp.h
+++ CommProto/architecture/os/os_udp.h
@@ -1,11 +1,14 @@
 #if (COM_TARGET_OS == COM_OS_WINDOWS)    
-    #include <Windows.h>
-    #pragma comment(lib,"ws2_32.lib")    
-#else
-    #include <unistd.h>
+    #include <Windows.h>	
+    #pragma comment(lib,"ws2_32.lib")
+	#define s_addr  S_un.S_addr 
+	#define closSocket(x)	closesocket(x); WSACleanup()
+#else    
     #include <sys/types.h>
     #include <sys/socket.h>
-    #include <netdb.h>
-    #include <stdint.h>
+    #include <netdb.h>    
     #include <string.h>
+	#include <arpa/net.h>
+	#define closSocket(x)	close(x)
 #endif
+
