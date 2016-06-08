5c5809059e567bd4ce7f8242c40b2100337d88d4
jessieEisenberg <jessieeisenberg@64JessieEisenberg64.Debian.net>
2016-06-08 00:21:29 -0700

    MASSIVE REORGANIZATION JUST FOR EASIER API USE

---
+++ build/CMakeFiles/NGCP_LIB.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/jessieeisenberg/Documents/GitHub/CommProtocol/CommProto/src/architecture/os/os_mutex.c" "/home/jessieeisenberg/Documents/GitHub/CommProtocol/build/CMakeFiles/NGCP_LIB.dir/CommProto/src/architecture/os/os_mutex.c.o"
+  "/home/jessieeisenberg/Documents/GitHub/CommProtocol/CommProto/src/architecture/os/os_threads.c" "/home/jessieeisenberg/Documents/GitHub/CommProtocol/build/CMakeFiles/NGCP_LIB.dir/CommProto/src/architecture/os/os_threads.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jessieeisenberg/Documents/GitHub/CommProtocol/CommProto/src/AbstractPacket.cpp" "/home/jessieeisenberg/Documents/GitHub/CommProtocol/build/CMakeFiles/NGCP_LIB.dir/CommProto/src/AbstractPacket.cpp.o"
+  "/home/jessieeisenberg/Documents/GitHub/CommProtocol/CommProto/src/comms.cpp" "/home/jessieeisenberg/Documents/GitHub/CommProtocol/build/CMakeFiles/NGCP_LIB.dir/CommProto/src/comms.cpp.o"
+  "/home/jessieeisenberg/Documents/GitHub/CommProtocol/CommProto/src/main.cpp" "/home/jessieeisenberg/Documents/GitHub/CommProtocol/build/CMakeFiles/NGCP_LIB.dir/CommProto/src/main.cpp.o"
+  "/home/jessieeisenberg/Documents/GitHub/CommProtocol/CommProto/src/network/UDP.cpp" "/home/jessieeisenberg/Documents/GitHub/CommProtocol/build/CMakeFiles/NGCP_LIB.dir/CommProto/src/network/UDP.cpp.o"
+  "/home/jessieeisenberg/Documents/GitHub/CommProtocol/CommProto/src/tools/hash/NgcpHash.cpp" "/home/jessieeisenberg/Documents/GitHub/CommProtocol/build/CMakeFiles/NGCP_LIB.dir/CommProto/src/tools/hash/NgcpHash.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../CommProto/include"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ build/CMakeFiles/NGCP_LIB.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NGCP_LIB.dir/CommProto/src/architecture/os/os_threads.c.o"
+  "CMakeFiles/NGCP_LIB.dir/CommProto/src/architecture/os/os_mutex.c.o"
+  "CMakeFiles/NGCP_LIB.dir/CommProto/src/AbstractPacket.cpp.o"
+  "CMakeFiles/NGCP_LIB.dir/CommProto/src/comms.cpp.o"
+  "CMakeFiles/NGCP_LIB.dir/CommProto/src/network/UDP.cpp.o"
+  "CMakeFiles/NGCP_LIB.dir/CommProto/src/tools/hash/NgcpHash.cpp.o"
+  "CMakeFiles/NGCP_LIB.dir/CommProto/src/main.cpp.o"
+  "NGCP_LIB.pdb"
+  "NGCP_LIB"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/NGCP_LIB.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
