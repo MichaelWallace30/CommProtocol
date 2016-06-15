749ada57c4ca55ee09b7fb17e908bfda97d8ec2b
CheezBoiger <alexander.garcia18@yahoo.com>
2016-06-15 00:13:13 -0700

    Testing for hash algorithm

--- CommProto/src/main.cpp
+++ CommProto/src/main.cpp
@@ -2,9 +2,48 @@
 #include <CommProto/comms.h>
 #include <CommProto/architecture/os/os_threads.h>
 #include <CommProto/architecture/os/include_defines.h>
+#include <CommProto/tools/hash/NgcpHash.h>
+
+using namespace Comnet::Tools::Hash;
+
+int testHash() {
+  unsigned long long start = 5000000;
+  unsigned long long end = 10000000;
+  int counter = 0;
+  for(unsigned long long i = start; i < end; ++i) {
+    ngcp_hash64_t hash = ngcp_hash64(&i, 8, 0);
+    for(unsigned long long j = start; j < end; ++j) {
+      ngcp_hash64_t test = ngcp_hash64(&j, 8, 0);
+      if(test == hash) {
+        if(i != j) {
+          std::cout << "COLLISION: i: " << i << " j: " << j << std::endl;
+          counter++;
+        }
+      }
+    }
+  }
+  printf("FINISHED\n# OF COLLISIONS: %lu\n", counter);
+  printf("COLLISION CHANGE : %f%%", (counter / (end - start))); 
+  return 0;
+}
+
+int testCharHash() {
+  std::string word = "CommProtPackfwjiofwoifwoijfoieifwoeifoijweoijfwoijfoijewoewfwefwfwfweetwefwefwefwefewfwefww";
+  std::string test = "AbstractPackeweifowefweiojfijoweiofioweioiojewfwefwefwefwetwefwfe";
+  uint64_t wordHash = ngcp_hash64((void*)word.c_str(), word.size(), 0);
+  uint64_t testHash = ngcp_hash64((void*)test.c_str(), test.size(), 0);
+  std::cout << wordHash << std::endl;
+  std::cout << testHash << std::endl;
+
+  if (wordHash == testHash) {
+    std::cout << "COLLISION" << std::endl;
+  }
+  return 0;
+} 
 
 int main(int c, char** args) {
-  printf("Hello World!");
+  printf("Hello World!\n");
+  testCharHash();
 /*
   Comms newComms1 = Comms(1);
   Comms newComms2 = Comms(2);
