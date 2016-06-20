1c3c5f05aafa3069437dfc25cf2715d8ba191e4e
CheezBoiger <alexander.garcia18@yahoo.com>
2016-06-19 19:55:39 -0700

    CMake changes

--- cmake/CommProto-config.cmake
+++ cmake/CommProto-config.cmake
@@ -8,13 +8,13 @@ set(COMMPROTO_HEADERS
   ${PARENT_HEADER_DIR}/CommsPacket.h
   ${PARENT_HEADER_DIR}/network/UDP.h
   ${PARENT_HEADER_DIR}/network/Serial.h
-  ${PARENT_HEADER_DIR}/network/TcpProtocol.h)
+  ${PARENT_HEADER_DIR}/network/TcpSocket.h)
 
 set(COMMPROTO_SOURCES
   ${PARENT_SOURCE_DIR}/AbstractPacket.cpp
   ${PARENT_SOURCE_DIR}/comms.cpp
   ${PARENT_SOURCE_DIR}/network/UDP.cpp
   ${PARENT_SOURCE_DIR}/network/Serial.cpp
-  ${PARENT_SOURCE_DIR}/network/TcpProtocol.cpp)		
+  ${PARENT_SOURCE_DIR}/network/TcpSocket.cpp)		
 
 
