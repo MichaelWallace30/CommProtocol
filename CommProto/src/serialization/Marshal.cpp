deaf32bf61392ec6aa27eb43aa9f715f54e33fa0
MicahaelWallace30 <Michael.Wallace30@gmail.com>
2016-06-27 22:46:55 -0700

    Working on serialization
    not finished don't judge me

--- CommProto/src/serialization/Marshal.cpp
+++ CommProto/src/serialization/Marshal.cpp
@@ -1,22 +1,70 @@
 #include <CommProto/serialization/Marshal.h>
+#include <stdlib.h>
+#include <algorithm>
+
+
 
 namespace Comnet {
 namespace Serialization {
 
-marshall_t packString(string_t data, int16 len, marshall_t input) {
-  
+	template <typename T>
+	void swap_endian(T& pX)
+	{
+		// should static assert that T is a POD
+
+		char& raw = reinterpret_cast<char&>(pX);
+		std::reverse(&raw, &raw + sizeof(T));
+	}
+
+	template <typename T>
+	T swap_endian_copy(T pX)
+	{
+		swap_endian(pX);
+		return pX;
+	}
+
+uint32_t packString(string_t data, int16_t len, marshall_t input) 
+{
+	
+	return NULL;
 }
 
-string_t unpackString(marshall_t input, int16 len) {
-
+string_t unpackString(marshall_t input, int16_t len)
+{
+	return NULL;
 }
 
-marshall_t packByte(uint8 data, marshall_t input) {
-
+uint32_t packByte(uint8_t data, marshall_t input) 
+{
+	return NULL;
 }
 
-uint8 unpackByte(marshall_t input) {
-
+uint32_t packUint16(uint16_t data, marshall_t input)
+{
+	swap_endian(data);
+	memcpy(input, &data, sizeof(uint32_t));
+	return sizeof(uint32_t);
 }
+
+uint16_t unpackUint16(marshall_t input)
+{
+	uint16_t value;
+	memcpy(&value, input, 2);
+	swap_endian(value); 
+	return value;
+}
+
+uint32_t packInt16(int16_t data, marshall_t input)
+{
+	return NULL;
+}
+
+int16_t unpackInt16(marshall_t input)
+{
+	return NULL;
+}
+
+
+
 } // Serialization namespace
 } // Comnet namespace
