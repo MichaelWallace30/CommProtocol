d3049fa98d708e050fb06774982cd7cb61ab281c
MichaelWallace30 <Michael.Wallace30@gmail.com>
2016-06-29 18:51:53 -0700

    Created generic pack and unpack
    needs to check to see if data type is primitive.

--- CommProto/include/CommProto/serialization/Marshal.h
+++ CommProto/include/CommProto/serialization/Marshal.h
@@ -1,6 +1,9 @@
 #ifndef __MARSHAL_H
 #define __MARSHAL_H
 
+
+#define LITTLE_ENDIAN_COMNET
+
 #include <CommProto\architecture\os\include_defines.h>
 
 namespace Comnet {
@@ -8,14 +11,17 @@ namespace Serialization {
 
 typedef uint8_t* marshall_t;
 typedef char* string_t;
+typedef wchar_t* wideString_t;
 
 typedef float real32_t;
 typedef double real64_t;
 
-// Marshalling values.
 
 uint32_t packString(string_t data, int16_t len, marshall_t input);
 string_t unpackString(marshall_t input, int16_t len);
+
+uint32_t packWideString(wideString_t data, int16_t len, marshall_t input);
+wideString_t unpackWideString(marshall_t input, int16_t len);
 
 uint32_t packByte(uint8_t data, marshall_t input);
 uint8_t unpackByte(marshall_t input);
@@ -43,6 +49,43 @@ real32_t unpackReal32(marshall_t input);
 
 uint32_t packReal64(real64_t, marshall_t input);
 real64_t unpackReal64(marshall_t input);
+
+
+template <typename T>
+void swap_endian(T& pX)
+{
+#ifdef LITTLE_ENDIAN_COMNET
+	// should static assert that T is a POD
+	char& raw = reinterpret_cast<char&>(pX);
+	std::reverse(&raw, &raw + sizeof(T));
+#endif
+}
+
+template <typename T>
+T swap_endian_copy(T pX)
+{
+	swap_endian(pX);
+	return pX;
+}
+
+
+template <typename T>
+int16_t packGeneric(T data, marshall_t input)
+{
+	swap_endian(data);
+	memcpy(input, &data, sizeof(T));
+	return sizeof(T);	
+}
+
+template <typename T>
+T unpackGeneric(marshall_t input)
+{
+	T value;
+	memcpy(&value, input, sizeof(T));
+	swap_endian(value);
+	return value;
+}
+
 } // Serialization namespace
 } // Comnet namespace
 #endif // __MARSHAL_H
