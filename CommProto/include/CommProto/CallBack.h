a45189cc59024e1d9ad08df4e1c0ca559d59a041
jessieEisenberg <jessieeisenberg@64JessieEisenberg64.Debian.net>
2016-07-21 15:38:02 -0700

    Still working on Callback.

---
+++ CommProto/include/CommProto/CallBack.h
@@ -0,0 +1,31 @@
+#ifndef __CALLBACK_H
+#define __CALLBACK_H
+
+#include <CommProto/architecture/os/include_defines.h>
+#include <CommProto/AbstractPacket.h>
+#include <CommProto/CommsPacket.h>
+
+namespace Comnet {
+
+
+typedef int error_t
+typedef error_t (*callback_t)(const header_t&, const AbstractPacket&);
+
+
+class Callback {
+public:
+  Callback();
+  Callback(callback_t* call);
+
+  ~Callback();
+
+  void setCallbackListener(callback_t* call);
+  /**
+     Calls the function associated with a packet.
+   */
+  error_t callFunction(const header_t& header, const AbstractPacket& abPacket);
+private:
+  callback_t* callback;
+};
+} // namespace Comnet
+#endif // __CALLBACK_H
