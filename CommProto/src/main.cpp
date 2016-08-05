1eace1ba8fd2c8f0fef7798581918ad5645172ca
CheezBoiger <alexander.garcia18@yahoo.com>
2016-08-05 15:47:51 -0700

    Added main update for unix test

--- CommProto/src/main.cpp
+++ CommProto/src/main.cpp
@@ -10,19 +10,68 @@
 #include <CommProto/serialization/ObjectStream.h>
 #include <CommProto/Callback.h>
 #include <CommProto/Packets.h>
+#include <CommProto/pkg/PacketManager.h>
 #include <stdlib.h>
 
 #include <iostream>
 using namespace std;
 using namespace Comnet::Serialization;
 using namespace Comnet;
+using namespace Comnet::Pkg;
 
 int apple(const header_t& gho, const Ping& pl) {
+  cout << "I got an apple" << endl;
   return pl.num;
 }
 
 int main(int c, char** args) {
-       
+  Ping d(12);
+  Ping a(122);
+	header_t head;
+  Callback call;
+	call.setCallbackListener((callback_t)apple);
+	cout << "Call: " << call.callFunction(head, d) << endl;
+
+  PacketManager packageManager;
+  // Sample test of storing Ping Callback pair. Should be done this way.
+  packageManager.insert(new Ping(0), new Callback((callback_t)apple));
+  Callback* callResult = packageManager.get(d);
+  int value = callResult->callFunction(head, a);
+  cout << "This is the result... " << value << endl;
+  cin.ignore();
+
+	ObjectStream newObjectStream = ObjectStream();
+
+	double value1A = 64.2;
+	int value2A = 12;
+	string_t strData = (char*)"Hello Slim!!!";
+	uint8_t valueA = 1;
+
+	std::wstring wStrA = L"Hello World Wide????";
+
+	newObjectStream << valueA << strData << wStrA << value2A << value1A;
+
+	double value1B;
+	int value2B;
+	uint8_t valueB;
+	std::wstring wStrB;
+	string_t strDataB = (string_t)malloc(25);
+
+	//extra varible
+	int extraVar;
+
+	newObjectStream >> value1B >> value2B >> wStrB >> strDataB >> valueB;
+
+    cout<<(int)valueB<<endl;
+    cout<<value1B<<endl;
+    cout << value2B <<endl;
+    cout << strDataB <<endl;
+    wcout << wStrB << endl;
+
+	//prints error does not crash
+	newObjectStream >> extraVar;
+
+	/*
   printf("Hello World!\n");
   Comms newComms1 = Comms(1);
   Comms newComms2 = Comms(2);
@@ -49,6 +98,6 @@ int main(int c, char** args) {
 	    Sleep(1000);
 		newComms2.receive(source, message);
 	}
-  
+  */
   return 0;
 }
