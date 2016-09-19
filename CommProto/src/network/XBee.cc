4f94b3d9b46732da1f541333a53a17b563c9eb59
CheezBoiger <alexander.garcia18@yahoo.com>
2016-09-19 01:23:22 -0700

    Fixed stupid tabs

--- CommProto/src/network/XBee.cc
+++ CommProto/src/network/XBee.cc
@@ -17,11 +17,11 @@ uint16_t profile_id = 0x0105;		// ZBA
 
 const zcl_comm_startup_param_t zcl_comm_default_sas =
 {
-  0xFFFE,											// short_address
-  ZCL_COMM_GLOBAL_EPID,						// extended_panid
-  0xFFFF,											// panid (0xFFFF = not joined)
+  0xFFFE,	                    // short_address
+  ZCL_COMM_GLOBAL_EPID,       // extended_panid
+  0xFFFF,	                    // panid (0xFFFF = not joined)
   UINT32_C(0x7FFF) << 11,						// channel_mask
-  0x02,												// startup_control
+  0x02,                     // startup_control
                               /*
                               { { 0, 0, 0, 0, 0, 0, 0, 0 } },			// trust_ctr_addr
                               { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
@@ -29,23 +29,23 @@ const zcl_comm_startup_param_t zcl_comm_default_sas =
                               */
   {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
   // network_key
-  FALSE,											// use_insecure_join
+  FALSE,                      // use_insecure_join
                               //	{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
   { 'C', 'a', 'l', '0', 'P', 'o', 'l', 'y', '0', 'P', 'o', 'm', 'o', 'n', 'a', '0' },
   // preconfig_link_key
-  0x00,												// network_key_seq_num
-  ZCL_COMM_KEY_TYPE_STANDARD,				// network_key_type
-  0x0000,											// network_mgr_addr
+  0x00,                         // network_key_seq_num
+  ZCL_COMM_KEY_TYPE_STANDARD,   // network_key_type
+  0x0000,	                      // network_mgr_addr
 
-  ZCL_COMM_SCAN_ATTEMPTS_DEFAULT,			// scan_attempts
-  ZCL_COMM_TIME_BETWEEN_SCANS_DEFAULT,	// time_between_scans
-  ZCL_COMM_REJOIN_INTERVAL_DEFAULT,		// rejoin_interval
+  ZCL_COMM_SCAN_ATTEMPTS_DEFAULT,	    // scan_attempts
+  ZCL_COMM_TIME_BETWEEN_SCANS_DEFAULT,  // time_between_scans
+  ZCL_COMM_REJOIN_INTERVAL_DEFAULT,	  // rejoin_interval
   ZCL_COMM_MAX_REJOIN_INTERVAL_DEFAULT,	// max_rejoin_interval
 
   {
-    FALSE,											// concentrator.flag
+    FALSE,  // concentrator.flag
     ZCL_COMM_CONCENTRATOR_RADIUS_DEFAULT,	// concentrator.radius
-    0x00												// concentrator.discovery_time
+    0x00  // concentrator.discovery_time
   },
 };
 
