9f9fa895b3c8db9bd8dab19398aa751b715916b2
MichaelWallace30 <Michael.Wallace30@gmail.com>
2016-09-28 15:43:16 -0700

    dummy enecryption class

--- CommProto/include/CommProto/encryption/encryptionInterface.h
+++ CommProto/include/CommProto/encryption/encryptionInterface.h
@@ -1,3 +1,5 @@
+#ifndef ENCRYPTION_INTERFACE_H
+#define ENCRYPTION_INTERFACE_H
 
 #include <stdint.h>
 #define BLOCK_SIZE 16
@@ -5,16 +7,17 @@
 //abstract class to use for aes encryption
 class encryptionInterface
 {
-	/** input c string as the form of encrytion key*/
-	virtual uint8_t loadKey(char* key) = 0;
-	/** load file which contatins the encryption key by the file name*/
-	virtual uint8_t loadKeyFromFile(char*keyFileName) = 0;
-	/** Encrypt buffer for desired length of data stream and return any agumented legnth by reference
-		A return value of - value is an error */
-	virtual int32_t encrypt(uint8_t* buffer, uint32_t& length, uint8_t IV[BLOCK_SIZE] = 0) = 0;
-	/** Decrypt buffer for desired length of data stream and return any agumented legnth by reference
-		A return value of - value is an error */
-	virtual int32_t decrypt(uint8_t* buffer, uint32_t& length, uint8_t IV[BLOCK_SIZE] = 0) = 0;
-	/** Randome numbder generator which fills an array of size legnth*/
-	virtual uint8_t generateRandomIV(uint8_t * buffer, uint32_t length)=0;
+/** input c string as the form of encrytion key*/
+virtual uint8_t loadKey(char* key) = 0;
+/** load file which contatins the encryption key by the file name*/
+virtual uint8_t loadKeyFromFile(char*keyFileName) = 0;
+/** Encrypt buffer for desired length of data stream and return any agumented legnth by reference
+	A return value of - value is an error */
+virtual int32_t encrypt(uint8_t* buffer, uint32_t& length, uint8_t IV[BLOCK_SIZE] = 0) = 0;
+/** Decrypt buffer for desired length of data stream and return any agumented legnth by reference
+	A return value of - value is an error */
+virtual int32_t decrypt(uint8_t* buffer, uint32_t& length, uint8_t IV[BLOCK_SIZE] = 0) = 0;
+/** Randome numbder generator which fills an array of size legnth*/
+virtual uint8_t generateRandomIV(uint8_t * buffer, uint32_t length)=0;
 };
+#endif
--- cmake/encryption.cmake
+++ cmake/encryption.cmake
@@ -4,7 +4,9 @@ set(ENCRYPTION_SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/CommProto/src/encryption/)
 
 
 set(ENCRYPTION_HEADERS
-  ${ENCRYPTION_HEADER_DIR}encryptionInterface.h)
+  ${ENCRYPTION_HEADER_DIR}encryptionInterface.h
+  ${ENCRYPTION_HEADER_DIR}aesEncryption.h)
   
 
-set(ENCRYPTION_SOURCES)
+set(ENCRYPTION_SOURCES
+${ENCRYPTION_SOURCE_DIR}aesEncryption.cpp)
