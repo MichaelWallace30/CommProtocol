32f4aea25b49b92f12df42c2d1e0df93b4514b76
MichaelWallace30 <Michael.Wallace30@gmail.com>
2016-09-28 15:27:31 -0700

    cmake encryption folder & files

---
+++ cmake/encryption.cmake
@@ -0,0 +1,10 @@
+# Tools config files, which are added into a set for compilation
+set(ENCRYPTION_HEADER_DIR ${CMAKE_CURRENT_SOURCE_DIR}/CommProto/include/CommProto/encryption/)
+set(ENCRYPTION_SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/CommProto/src/encryption/)
+
+
+set(ENCRYPTION_HEADERS
+  ${ENCRYPTION_HEADER_DIR}encryptionInterface.h)
+  
+
+set(ENCRYPTION_SOURCES)
