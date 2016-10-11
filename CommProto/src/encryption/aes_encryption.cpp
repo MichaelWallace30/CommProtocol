a0bd43c4f6eb1b250048c25d580282ac6df4db1f
MicahaelWallace30 <Michael.Wallace30@gmail.com>
2016-10-10 22:04:21 -0700

    Encryption and random generator for IV

--- CommProto/src/encryption/aes_encryption.cpp
+++ CommProto/src/encryption/aes_encryption.cpp
@@ -1,10 +1,14 @@
-#include <CommProto/encryption/aes_encryption.h>
+#include <CommProto/encryption/aes_encryption.h>//header file
 #include <CommProto/architecture/macros.h>//str_length
 #include <CommProto/debug/comms_debug.h>//COMMS_DEBUG
+#include <fstream>//file input
 
+using namespace comnet;
 
-AesEncryption::AesEncryption(){}
+AesEncryption::AesEncryption() :randomGen(0, 255){}
+
 AesEncryption::~AesEncryption(){}
+
 /** input c string as the form of encrytion key*/
 uint8_t AesEncryption::LoadKey(char* key){	
 	byte temp_key[KEY_LENGTH] = { 0 };
@@ -20,10 +24,33 @@ uint8_t AesEncryption::LoadKey(char* key){
 	}
 	return 1;//success
 }
+
 /** load file which contatins the encryption key by the file name*/
 uint8_t AesEncryption::LoadKeyFromFile(char*keyFileName){
-	//@TODO Michael Wallace
-	return 1; }
+	std::ifstream keyFileInput(keyFileName);//open file
+	std::string inputString;//string to read line from file
+	char temp_key[KEY_LENGTH];//temp key to be inserted from file line
+
+	if (keyFileInput.is_open()){
+		std::getline(keyFileInput, inputString);//get line
+
+		if (inputString.length() >= KEY_LENGTH){//translate line of file to key length as char*
+			for (int x = 0; x < KEY_LENGTH; x++){
+				temp_key[x] = inputString[x];
+			}
+		}
+		else{//key length is too small
+			COMMS_DEBUG("key.txt characters mismatch.\nCharacters found: %d\nCharacters needed: %d\n", inputString.length(), KEY_LENGTH);
+			return 0;
+		}
+	}
+	else{//file not open	
+		COMMS_DEBUG("%s not found. \n", temp_key);
+		return 0;
+	}
+	keyFileInput.close();
+	return LoadKey(temp_key);//load key from temp key
+}
 
 /** Encrypt buffer for desired length of data stream and return any agumented legnth by reference
 A return value of - value is an error */
@@ -42,7 +69,9 @@ int32_t AesEncryption::Decrypt(uint8_t* buffer, uint32_t& length, uint8_t iv[BLO
 }
 
 /** Randome numbder generator which fills an array of size legnth*/
-uint8_t AesEncryption::GenerateRandomIV(uint8_t * buffer, uint32_t length){
-	//@TODO Michael Wallace
+uint8_t AesEncryption::GenerateRandomIV(uint8_t * buffer, uint32_t length){	
+	for (int x = 0; x < length; x++){
+		buffer[x] = randomGen.randomUint8();
+	}
 	return 1;
 }
