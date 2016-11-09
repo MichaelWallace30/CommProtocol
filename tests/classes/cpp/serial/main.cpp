c32431324fa24dc09250152f1afbeac41bfea025
Jester565 <ajcraig543@gmail.com>
2016-11-08 18:11:57 -0800

    Added documentation to main.cpp

--- tests/classes/cpp/serial/main.cpp
+++ tests/classes/cpp/serial/main.cpp
@@ -1,14 +1,34 @@
-#include <iostream>
 #define __COMMS_DEBUG_FATAL_EXIT
 #include <CommProto/comms.h>
 #include <CommProto/abstractpacket.h>
 #include <CommProto/callback.h>
-#include <CommProto/network/serial.h>
-#include <unordered_map>
 #include <vector>
 #include <string>
-#include <thread>
+#include <iostream>
+/*
+#Note
+CommProtocol Library requires libxbee3 to execute copy the file to your bin folder (ie .exe)
+-Windows {build dir}\libxbee3\(debug or release)\libxbee3.dll
+-Linux: {build dir}\libxbee3\libxbee3.o
+*/
 
+/*
+#Note
+This examples requires that you have two COM ports on you computer that
+are connected to one another.  Most computers don't have RS 232 ports anymore.
+Check https://docs.google.com/document/d/1WCYMmVNjQgmBkN0y59NzXw2j078tIpOrO-Y1PAn8LAk/edit?usp=sharing for alternatives.
+*/
+
+//First COM port
+static const char* PORT1 = "COM5";
+
+//Second COM port
+static const char* PORT2 = "COM6";
+
+//Amount of milliseconds between sends
+static const int SEND_SLEEP_MILLIS = 4000;
+
+//Simple AbstractPacket that Packs and UnPacks a string.
 class Ping : INHERITS_ABSPACKET {
 public:
 		Ping()
@@ -46,6 +66,7 @@ private:
 		std::string data;
 };
 
+//Callback for Ping
 error_t PingCallback(const comnet::Header& header, const Ping& packet, comnet::Comms& node) {
 		std::cout << "=::RECEIVED PACKET::=" << std::endl;
 		std::cout << std::endl << "Source node: " << (int32_t)header.source_id << std::endl;
@@ -53,12 +74,6 @@ error_t PingCallback(const comnet::Header& header, const Ping& packet, comnet::C
 		std::cout << "Packet contains: " << packet.GetData() << std::endl;
 		return comnet::CALLBACK_SUCCESS | comnet::CALLBACK_DESTROY_PACKET;
 }
-
-static const char* PORT1 = "COM5";
-
-static const char* PORT2 = "COM6";
-
-static const int SEND_SLEEP_MILLIS = 4000;
 
 int main()
 {
@@ -75,14 +90,15 @@ int main()
 		}
 		comnet::architecture::os::CommMutex mut;
 		comnet::architecture::os::CommLock commlock(mut);
-		// This will cause the thread to wait for a few milliseconds, causing any other thread to wait.
-		comnet::architecture::os::WaitForMilliseconds(commlock, cond, 1000);
+		comnet::architecture::os::WaitForMilliseconds(commlock, cond, 1000);		//Stop this thread for 1 second
 
+		//Open the serial port to the value of PORT1
 		if (!comm1.InitConnection(SERIAL_LINK, PORT1, NULL, 9600))
 		{
 				std::cerr << "Unable to initialize port " << PORT1 << std::endl;
 		}
 
+		//Open the serial port the value of PORT2
 		if (!comm2.InitConnection(SERIAL_LINK, PORT2, NULL, 9600))
 		{
 				std::cerr << "Unable to initialize port " << PORT2 << std::endl;
